233e53d0033ed145b1b3ac186bde0e6ae9b4218c
IM-Tamim <imtamim1111@gmail.com>
2026-03-07 02:04:23 +0600

    dynamic card added with conditional rendering

--- script.js
+++ script.js
@@ -6,18 +6,23 @@ async function loadCards() {
     data.data.forEach(data => {
         const card = document.createElement('div');
         card.innerHTML =`
-        <div class="border-1 border-gray-200 border-t-4 border-t-green-500 rounded-lg p-4 shadow-xl h-full">
+        <div class="border-2 border-gray-200 border-t-4 rounded-lg p-4 shadow-xl h-full flex flex-col ${data.status === 'open' ? 'border-t-green-500' : 'border-t-purple-500'}">
           <div class="flex justify-between">
-              <span class="text-gray-400"><img src="./assets/Open-Status.png" alt=""></span>
-              <span class="text-xs font-medium text-red-600 bg-red-100 px-6 py-1 rounded-full">${data.priority}</span>
+              <span class="text-gray-400">
+                <img src="./assets/${data.status === 'open' ? 'Open-Status' : 'Closed-Status'}.png" alt="">
+              </span>
+              <span class="text-xs font-medium px-6 py-1 rounded-full uppercase ${
+                  data.priority === 'high' ? 'text-red-600 bg-red-100' : 
+                  data.priority === 'medium' ? 'text-yellow-600 bg-yellow-100' : 'text-gray-600 bg-gray-100'
+                }">${data.priority}</span>
           </div>
           
           <h3 class="font-semibold mt-2">${data.title}</h3>
           <p class="text-sm text-gray-600 mt-1 line-clamp-2">${data.description}</p>
           
-          <div class="flex gap-2 mt-3">
-              <span class="text-xs bg-red-100 text-red-600 px-2 py-0.5 rounded-full"><i class="fa-solid fa-bug"></i> BUG</span>
-              <span class="text-xs bg-yellow-100 text-yellow-600 px-2 py-0.5 rounded-full"><i class="fa-regular fa-life-ring"></i> HELP WANTED</span>
+          <div class="flex flex-wrap gap-2 mt-3">
+              ${data.labels.map(label => `<span class="text-xs bg-blue-100 text-blue-600 px-2 py-1 
+                rounded-full">${label}</span>`).join('')}
           </div>
           <hr class="text-gray-200 my-4" />
           <div class="flex justify-between text-[0.6rem] text-gray-400">
