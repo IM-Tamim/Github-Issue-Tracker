5cbeeb0d50573c2ad439e161bf6f4b8c75523a1c
IM-Tamim <imtamim1111@gmail.com>
2026-03-07 18:28:57 +0600

    file path corrected for github

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.getElementById("login-btn")
         const pass = document.getElementById("input_password").value;
         if (userName === "admin" && pass === "admin123") {
             alert("Sign In successful!");
-            window.location.assign("/home.html");
+            window.location.assign("./home.html");
         }else {
             alert("Sign In failed!!! Please check your Username & Password.");
             return;
